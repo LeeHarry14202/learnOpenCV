3a0c3479bd4d9266c2174d4656f7ae2be1f03f77
LeeHarry14202 <20521912@gm.uit.edu.vn>
2021-10-31 11:15:50 +0700

    Update

--- chapter_6.py
+++ chapter_6.py
@@ -1,11 +1,12 @@
 from utils import *
 
 img = cv.imread(lenaImg)
+img_1 = cv.imread(lamboImg)
 
 imgHor = np.hstack((img,img))
 imgVer = np.vstack((img,img))
 
-cv.imshow('Image Ver', imgVer)
-cv.imshow('Image Hor', imgHor)
+cv.imshow('Image Ver', rescaleFrame(imgVer, 0.5))
+cv.imshow('Image Hor', rescaleFrame(imgHor, 0.5))
 
 cv.waitKey(0)
